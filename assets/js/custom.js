2288b40e2a2655330a028cad37d4d5f5b5a42805
waseemusman17 <waseeem.usman17@gmail.com>
2020-11-05 11:36:39 +0500

    Some Functions add on functions.php file and setup wordpress theme

---
+++ assets/js/custom.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    // Home Page SLider
+    $('.cis-slider.owl-carousel').owlCarousel({
+        loop:true,
+        margin:50,
+        nav:true,
+        dots:false,
+        navText:["<img src='./assets/img/icon-left.svg' >","<img src='./assets/img/icon-right.svg' >"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+    
+
+    // Home Page Portfolio Slider
+    $('.pfs-slider .owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items:1,
+        dots:false,
+        navText: ["<div id='counter_banner'></div><img src='./assets/img/icon-left.svg' > ","<img src='./assets/img/icon-right.svg' >"],
+            onInitialized  : counter, //When the plugin has initialized.
+            onTranslated : counter //When the translation of the stage has finished.
+    });
+    
+    function counter(event) {
+        var element   = event.target;         // DOM element, in this example .owl-carousel
+        var items     = event.item.count;     // Number of items
+        var item      = event.item.index - 1;     // Position of the current item
+
+         // it loop is true then reset counter from 1
+        if(item > items) {
+            item = item - items
+        }
+        $('#counter_banner').html("<span>"+item+"</span> <span> / </span> <span>"+items+"</span>");
+    }
+
+  
+});
---
+++ functions.php
@@ -0,0 +1,49 @@
+<?php
+
+// To add Style Sheet
+function css_resourses(){
+    wp_enqueue_style('style' , get_stylesheet_rui() );
+}
+add_action('wp_enqueue_script','css_resourses');
+
+
+
+// Logo Code
+add_theme_support('custom-logo');
+add_theme_support('custom-logo' , array(
+    'height'      => 100,
+    'width'       => 100,
+    'flex-height' => true,
+    'flex-width'  => true,
+    'header-text' => array( 'site-title', 'site-description' ),
+));
+
+// Menu Bar
+function wpb_custom_new_menu(){
+    register_nav_menu(
+        array(
+            'Header-menu' => __('Header Menu')
+        )
+    );
+}
+
+add_action('init' , 'wpb_custom_new_menu');
+
+
+//   ACF Plugin Support
+add_action('acf/init', 'my_acf_op_init');
+function my_acf_op_init() {
+
+    // Check function exists.
+    if( function_exists('acf_add_options_page') ) {
+
+        // Register options page.
+        $option_page = acf_add_options_page(array(
+            'page_title'    => __('Theme General Settings'),
+            'menu_title'    => __('Theme Settings'),
+            'menu_slug'     => 'theme-general-settings',
+            'capability'    => 'edit_posts',
+            'redirect'      => false
+        ));
+    }
+} 
